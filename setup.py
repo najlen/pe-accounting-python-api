49447250d96201344177b8580849f212646252d3
Daniel Nihlén <daniel.nihlen@sdnit.se>
2022-01-30 11:55:42 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="pe-accounting-python-api",
-    version="0.5.5",
+    version="0.5.6",
     description="API bindings for https://www.accounting.pe/sv/",
     long_description=README,
     long_description_content_type="text/markdown",
